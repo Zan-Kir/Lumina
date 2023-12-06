115bae5edd6faf4866f5a736d47b497d187e3943
felipe6san <felipe6san@hotmail.com>
2023-12-05 23:11:19 -0300

    parceiros

--- script.js
+++ script.js
@@ -26,3 +26,14 @@
     }
   });
 }
+
+function troca(botao) {
+    // Remove a classe 'ativo' de todos os botões
+    var botoes = document.querySelectorAll('.botoesPrincipais button');
+    botoes.forEach(function (btn) {
+      btn.classList.remove('ativo');
+    });
+
+    // Adiciona a classe 'ativo' apenas ao botão clicado
+    botao.classList.add('ativo');
+}
